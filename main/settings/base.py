f03e8ff8b570437e922486fa67027f11387ec501
Mihail Tal <mihail.tal85@gmail.com>
2023-01-07 18:36:23 +0300

    django ready to use for django projects including custom serializer

--- main/settings/base.py
+++ main/settings/base.py
@@ -41,7 +41,10 @@ INSTALLED_APPS = [
     #my app
     'rest_framework',
     'drf_yasg',
+    "users",
     # 'debug_toolbar',
+    "rest_framework.authtoken",
+    "dj_rest_auth",
 
 ]
 
@@ -195,5 +198,15 @@ LOGGING = {
     },
 }
 # handlers iki farklı handler işlemi yapan loji kuran 
-console ve file için i,ki farklı handler kullanılmış
-consolda stream handler 
+# console ve file için i,ki farklı handler kullanılmış
+# consolda stream handler 
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
+
+REST_AUTH_SERIALIZERS ={
+    "TOKEN_SERIALIZER": "users.serializers.CustomTokenSerializer",
+}
